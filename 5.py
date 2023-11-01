f37d153650bef52f168dec827178fb3a2ff336d3
23Ilya23 <143864293+23Ilya23@users.noreply.github.com>
2023-11-01 22:31:38 +0700

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,10 @@
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+if a + b > c:
+   print("Треугольник существует")
+elif a + b < c:
+   print("Треугольник не существует")
+elif a + b == c:
+   print("Равно")
