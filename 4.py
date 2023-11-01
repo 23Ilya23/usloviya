ba941acc63e09aa666e5953a36b9aab330d5b879
23Ilya23 <143864293+23Ilya23@users.noreply.github.com>
2023-11-01 22:19:04 +0700

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+print("Координаты точки:")
+x = float(input("x = "))
+y = float(input("y = "))
+if x > 0 and y > 0:
+    print(f' {x} "и" {y} "Это 1 четверти"')
+elif x < 0 and y > 0:
+    print(f' {x} "и" {y} "Это 2 четверти"')
+elif x < 0 and y < 0:
+   print(f' {x} "и" {y} "Это 3 четверти"')
+elif x > 0 and y < 0:
+    print(f' {x} "и" {y} "Это 4 четверти"')
